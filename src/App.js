b6d48b11c3765a3f57b677f6a08221ed9e846d45
bhavana522 <67970804+bhavana522@users.noreply.github.com>
2021-04-04 23:56:07 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Particles from "react-particles-js";
 import About from "./pages/about/about.component";
 import Skills from "./pages/skills/skills.component";
+import ProjectTimeline from "./components/projects-timeline/project-line.component";
+import ContactForm from "./pages/contact-form/contact-form.component";
 import Navbar from "./components/Navbar";
 import Header from"./components/Header";
 const App  =()=> {
   return (
     <>
-    <Particles
-    params={{
-      particles:{
-        number:{
-          value:30,
-          density: {
-            enable:true,
-            value_area:900
-          }
-        },
-        shape:{
-          type:"polygon",
-          stroke:{
-            width:6,
-            color:"#f9ab00"
-          }
-        }
-      }
-    }}
+   
 
       
-      />
+    
     <Navbar/>
     <Header/>
     <About/>
     <Skills/>
-    
+    <ProjectTimeline/>
+    <ContactForm />
     </>
   );
 }
