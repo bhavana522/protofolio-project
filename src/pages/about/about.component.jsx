19d32765cf0ac6af15f7c4c20b93edaf77bde86a
bhavana522 <67970804+bhavana522@users.noreply.github.com>
2021-04-07 08:19:51 +0530

    init

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import './about.style.css'
-import Profile from '../about/B logo.jpg'
+import Profile from '../about/profile.jpeg'
 import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button'
 
